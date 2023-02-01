1898560943ef986d5a3f7a2531963b8a3ac072ac
Aaron Bassett <arbassett4@outlook.com>
2023-02-01 01:06:20 -0500

    none vendor

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
 // @ts-expect-error no typing
-import vercel from "./solid-start-vercel";
+import vercel from "solid-start-vercel";
   
 export default defineConfig(() => {
   return {
