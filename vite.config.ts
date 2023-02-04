c75d5e7af38b86b547c94b017662be9e7fe6b679
Aaron Bassett <arbassett4@outlook.com>
2023-02-04 12:42:18 -0500

    use netlify adapter

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
 // @ts-expect-error no typing
-import vercel from "solid-start-vercel";
+import netlify from "solid-start-netlify";
   
 export default defineConfig(() => {
   return {
-    plugins: [solid({ ssr: true, adapter: vercel({ edge: false }) })],
+    plugins: [solid({ ssr: true, adapter: netlify({ edge: false }) })],
   };
 });
   
